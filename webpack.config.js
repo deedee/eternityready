e5b395cfdc2f62e5a48079aa63c7c2904efa59c8
Daryl Browne <daryl.browne@googlemail.com>
2016-09-29 20:32:26 +0100

    webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const webpackPort = 3001
+const webpackConfig = {
+	resolve: {
+		extensions: ['', '.js']
+	},
+
+	entry: [
+		'webpack-dev-server/client?http://localhost:3001',
+		'webpack/hot/only-dev-server',
+		// 'webpack-hot-middleware/client?path=/app/__webpack_hmr&timeout=20000',
+		'./src/app/bootstrap.js'
+	],
+	output: {
+		path: path.join(__dirname, '/src/app/'),
+		publicPath: '/',
+		filename: 'app.bundle.js',
+	},
+
+	node: {
+		fs: 'empty'
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.png/,
+				loader: 'file'
+			},
+			{
+				test: /\.json$/,
+				loader: 'json'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react', 'stage-1']
+				}
+			}
+		]
+	},
+
+	plugins: [
+		// new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		// new webpack.NoErrorsPlugin()
+	]
+}
+
+module.exports = {
+	port: webpackPort,
+	config: webpackConfig
+}
