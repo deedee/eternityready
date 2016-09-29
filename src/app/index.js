e487a70a739074792284f7ea4906c8887210a853
Daryl Browne <daryl.browne@googlemail.com>
2016-09-29 23:22:45 +0100

    make index.js a stateless component

--- src/app/index.js
+++ src/app/index.js
@@ -1,35 +1,29 @@
 import React from 'react';
 
-class IndexTemplate extends React.Component{
-	render () {
-		return (
-				<html>
-					<head>
-						<title>{this.props.title}</title>
-						<link rel="stylesheet" href="/app/css/bootstrap.css" />
-						<link rel="stylesheet" href="/app/css/main.css" />
-						<link rel="stylesheet" href="/app/containers/Player.css" />
-					</head>
-					<body>
-						enemy
-						<div id="root" dangerouslySetInnerHTML={{__html: this.props.html}}></div>
-						<script
-							type="text/json"
-							id="serialisedState"
-							dangerouslySetInnerHTML={{__html: this.props.reduxState}}
-						/>
-						<script
-							type="text/json"
-							id="serverProps"
-							dangerouslySetInnerHTML={{__html: JSON.stringify(this.props)}}
-						/>
-						<script src="/webpack-dev-server.js"></script>
-						<script src="/app/app.bundle.js"></script>
-					</body>
-				</html>
-			)
-	}
-}
-
+const IndexTemplate = () => (
+	<html>
+		<head>
+			<title>{this.props.title}</title>
+			<link rel="stylesheet" href="/app/css/bootstrap.css" />
+			<link rel="stylesheet" href="/app/css/main.css" />
+			<link rel="stylesheet" href="/app/containers/Player.css" />
+		</head>
+		<body>
+			<div id="root" dangerouslySetInnerHTML={{__html: this.props.html}}></div>
+			<script
+				type="text/json"
+				id="serialisedState"
+				dangerouslySetInnerHTML={{__html: this.props.reduxState}}
+			/>
+			<script
+				type="text/json"
+				id="serverProps"
+				dangerouslySetInnerHTML={{__html: JSON.stringify(this.props)}}
+			/>
+			<script src="/webpack-dev-server.js"></script>
+			<script src="/app/app.bundle.js"></script>
+		</body>
+	</html>
+)
 
 export default IndexTemplate;
